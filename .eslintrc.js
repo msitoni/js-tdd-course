f7fa37d084481f325d5869b24de557e2c09cf194
marcio <msitoni@hotmail.com>
2019-08-16 14:02:38 -0300

    Instalação do eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     "env": {
         "browser": true,
+        "commonjs": true,
         "es6": true,
         "node": true
     },
@@ -10,25 +11,8 @@ module.exports = {
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+        "ecmaVersion": 2018
     },
     "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
     }
 };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 const a = 3;
 const b = 2;
-const c = 'asdf asdf asdfasdfasdfasdfasdfasdfasdfasdfasdf asdf asdf asdfasdf asdfa sdfasdf asdf';
+const c = "asdf asdf asdfasdfasdfasdfasdfasdfasdfasdfasdf asdf asdf asdfasdf asdfa sdfasdf asdf";
 
 function sum() {
     return a + b + c;
 }
 
-sum();
+sum()
